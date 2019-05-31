c1bcabc3a7d5031d21ac8fbb9618cf72e237540a
Ivan <i.nikolov.90@gmail.com>
2019-05-31 22:48:45 +0300

    rewrite json serialization to be less hacky

--- main.go
+++ main.go
@@ -11,62 +11,96 @@ import (
 
 var port = flag.Int("port", 8080, "")
 
-type translation struct {
-	English string
-	Gopher  string
-}
-
-func (t *translation) UnmarshalJSON(b []byte) error {
-	in := struct {
-		Word string `json:"english-word"`
-	}{}
-	if err := json.Unmarshal(b, &in); err != nil {
-		return err
-	}
-	t.English = in.Word
-	return nil
-}
-
-func (t translation) MarshalJSON() ([]byte, error) {
-	out := struct {
-		Word string `json:"gopher-word"`
-	}{Word: t.Gopher}
-	return json.Marshal(out)
-}
-
-type history []translation
-
-var hr history
-
 func handleWord(rw http.ResponseWriter, req *http.Request) {
 	body, err := ioutil.ReadAll(req.Body)
 	if err != nil {
-		log.Fatal(err)
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
-	var tr translation
-
-	err = json.Unmarshal(body, &tr)
+	var english, gopher string
+	err = json.Unmarshal(body, &struct {
+		Word *string `json:"english-word"`
+	}{
+		Word: &english,
+	})
 	if err != nil {
-		log.Fatal(err)
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
 	}
-	tr.Gopher = TranslateWord(tr.English)
 
-	hr = append(hr, tr)
+	if len(english) == 0 {
+		http.Error(rw, "No english word provided.", http.StatusBadRequest)
+		return
+	}
+	gopher = TranslateWord(english)
 
-	err = json.NewEncoder(rw).Encode(tr)
+	data, err := json.Marshal(struct {
+		Word *string `json:"gopher-word"`
+	}{
+		Word: &gopher,
+	})
 	if err != nil {
-		log.Fatal(err)
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
+	_, err = rw.Write(data)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func handleSentence(rw http.ResponseWriter, req *http.Request) {
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var english, gopher string
+	err = json.Unmarshal(body, &struct {
+		Word *string `json:"english-sentence"`
+	}{
+		Word: &english,
+	})
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if len(english) == 0 {
+		http.Error(rw, "No english sentence provided.", http.StatusBadRequest)
+		return
+	}
+
+	gopher = TranslateSentence(english)
+
+	data, err := json.Marshal(struct {
+		Word *string `json:"gopher-sentence"`
+	}{
+		Word: &gopher,
+	})
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	_, err = rw.Write(data)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func main() {
 	flag.Parse()
 
 	http.HandleFunc("/word", handleWord)
-	http.HandleFunc("/sentence", handleWord)
+	http.HandleFunc("/sentence", handleSentence)
 	http.HandleFunc("/history”", handleWord)
 
+	log.Println("Start server on ", *port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
 }
