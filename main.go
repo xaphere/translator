fbd677eb2e53affb7e5f1c6c38bddc5caf1e2c7a
Ivan Nikolov <inikolov90@gmail.com>
2019-06-02 08:04:37 +0300

    extract http server from main

--- main.go
+++ main.go
@@ -1,136 +1,17 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
-	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
 )
 
 var port = flag.Int("port", 8080, "")
-var history History
-
-func handleWord(rw http.ResponseWriter, req *http.Request) {
-	if req.Method != "POST" {
-		http.Error(rw, "", http.StatusMethodNotAllowed)
-		return
-	}
-	body, err := ioutil.ReadAll(req.Body)
-	if err != nil {
-		http.Error(rw, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	var english, gopher string
-	err = json.Unmarshal(body, &struct {
-		Word *string `json:"english-word"`
-	}{
-		Word: &english,
-	})
-	if err != nil {
-		http.Error(rw, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	if len(english) == 0 {
-		http.Error(rw, "No english word provided.", http.StatusBadRequest)
-		return
-	}
-	gopher, err = TranslateWord(english)
-	history.Store(english, gopher)
-
-	data, err := json.Marshal(struct {
-		Word *string `json:"gopher-word"`
-	}{
-		Word: &gopher,
-	})
-	if err != nil {
-		http.Error(rw, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	_, err = rw.Write(data)
-	if err != nil {
-		http.Error(rw, err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
-
-func handleSentence(rw http.ResponseWriter, req *http.Request) {
-	if req.Method != "POST" {
-		http.Error(rw, "", http.StatusMethodNotAllowed)
-		return
-	}
-	body, err := ioutil.ReadAll(req.Body)
-	if err != nil {
-		http.Error(rw, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	var english, gopher string
-	err = json.Unmarshal(body, &struct {
-		Word *string `json:"english-sentence"`
-	}{
-		Word: &english,
-	})
-	if err != nil {
-		http.Error(rw, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	if len(english) == 0 {
-		http.Error(rw, "No english sentence provided.", http.StatusBadRequest)
-		return
-	}
-
-	gopher, err = TranslateSentence(english)
-	history.Store(english, gopher)
-
-	data, err := json.Marshal(struct {
-		Word *string `json:"gopher-sentence"`
-	}{
-		Word: &gopher,
-	})
-	if err != nil {
-		http.Error(rw, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	_, err = rw.Write(data)
-	if err != nil {
-		http.Error(rw, err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
-
-func handleHistory(rw http.ResponseWriter, req *http.Request) {
-	if req.Method != "GET" {
-		http.Error(rw, "", http.StatusMethodNotAllowed)
-		return
-	}
-
-	data, err := history.ToJSON()
-	if err != nil {
-		http.Error(rw, err.Error(), http.StatusInternalServerError)
-		return
-
-	}
-	_, err = rw.Write(data)
-	if err != nil {
-		http.Error(rw, err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
 
 func main() {
 	flag.Parse()
 
-	http.HandleFunc("/word", handleWord)
-	http.HandleFunc("/sentence", handleSentence)
-	http.HandleFunc("/history", handleHistory)
-
-	log.Println("Start server on ", *port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
+	err := initServer(*port)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
---
+++ server.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func handleWord(rw http.ResponseWriter, req *http.Request) *serverError {
+
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		return &serverError{Error: err, Msg: err.Error(), Code: http.StatusInternalServerError}
+	}
+
+	var english, gopher string
+	err = json.Unmarshal(body, &struct {
+		Word *string `json:"english-word"`
+	}{
+		Word: &english,
+	})
+	if err != nil {
+		return &serverError{Error: err, Msg: err.Error(), Code: http.StatusInternalServerError}
+	}
+
+	if len(english) == 0 {
+		return &serverError{Error: nil, Msg: "No english word provided.", Code: http.StatusBadRequest}
+	}
+
+	gopher, err = TranslateWord(english)
+	history.Store(english, gopher)
+
+	data, err := json.Marshal(struct {
+		Word *string `json:"gopher-word"`
+	}{
+		Word: &gopher,
+	})
+	if err != nil {
+		return &serverError{Error: err, Msg: err.Error(), Code: http.StatusInternalServerError}
+	}
+
+	_, err = rw.Write(data)
+	if err != nil {
+		return &serverError{Error: err, Msg: err.Error(), Code: http.StatusInternalServerError}
+	}
+
+	return nil
+}
+
+func handleSentence(rw http.ResponseWriter, req *http.Request) *serverError {
+
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		return &serverError{Error: err, Msg: err.Error(), Code: http.StatusInternalServerError}
+	}
+
+	var english, gopher string
+	err = json.Unmarshal(body, &struct {
+		Word *string `json:"english-sentence"`
+	}{
+		Word: &english,
+	})
+	if err != nil {
+		return &serverError{Error: err, Msg: err.Error(), Code: http.StatusInternalServerError}
+	}
+
+	if len(english) == 0 {
+		return &serverError{Error: nil, Msg: "No english sentence provided.", Code: http.StatusBadRequest}
+	}
+
+	gopher, err = TranslateSentence(english)
+	history.Store(english, gopher)
+
+	data, err := json.Marshal(struct {
+		Word *string `json:"gopher-sentence"`
+	}{
+		Word: &gopher,
+	})
+	if err != nil {
+		return &serverError{Error: err, Msg: err.Error(), Code: http.StatusInternalServerError}
+	}
+
+	_, err = rw.Write(data)
+	if err != nil {
+		return &serverError{Error: err, Msg: err.Error(), Code: http.StatusInternalServerError}
+	}
+	return nil
+}
+
+var history History
+
+func handleHistory(rw http.ResponseWriter, req *http.Request) *serverError {
+
+	data, err := history.ToJSON()
+	if err != nil {
+		return &serverError{Error: err, Msg: err.Error(), Code: http.StatusInternalServerError}
+
+	}
+	_, err = rw.Write(data)
+	if err != nil {
+		return &serverError{Error: err, Msg: err.Error(), Code: http.StatusInternalServerError}
+	}
+	return nil
+}
+
+type serverError struct {
+	Error error
+	Msg   string
+	Code  int
+}
+
+type serverHandler struct {
+	Handle func(w http.ResponseWriter, r *http.Request) *serverError
+	Method string
+}
+
+func (sh serverHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != sh.Method {
+		http.Error(w, fmt.Sprintf("%s method not supported", r.Method), http.StatusMethodNotAllowed)
+		return
+	}
+
+	if err := sh.Handle(w, r); err != nil {
+		http.Error(w, err.Msg, err.Code)
+	}
+}
+
+func initServer(port int) error {
+	http.Handle("/word", serverHandler{Handle: handleWord, Method: "POST"})
+	http.Handle("/sentence", serverHandler{Handle: handleSentence, Method: "POST"})
+	http.Handle("/history", serverHandler{Handle: handleHistory, Method: "GET"})
+
+	log.Println("Start server on ", port)
+	strPort := fmt.Sprintf(":%d", port)
+	err := http.ListenAndServe(strPort, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
